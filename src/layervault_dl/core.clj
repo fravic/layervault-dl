92d791480c02c74c4bcec18a40e04320112c63ce
Fravic <fravicf@gmail.com>
2015-03-22 14:16:23 -0700

    Get down to revision ids

--- src/layervault_dl/core.clj
+++ src/layervault_dl/core.clj
@@ -4,6 +4,27 @@
             [clojure.tools.cli :refer [parse-opts]]))
 
 
+;; LayerVault API parsing
+
+(defn get-all-links
+  [data object link]
+  (get-in data [object 0 "links" link]))
+
+(defn get-last-link
+  [data object link]
+  [(last (get-in data [object 0 "links" link]))])
+
+(defn user-org-links [data] (get-all-links data "users" "organizations"))
+(defn org-project-links [data] (get-all-links data "organizations" "projects"))
+(defn project-folder-links [data] (get-all-links data "projects" "folders"))
+(defn project-file-links [data] (get-all-links data "projects" "files"))
+(defn folder-folder-links [data] (get-all-links data "folders" "folders"))
+(defn folder-file-links [data] (get-all-links data "folders" "files"))
+(defn file-revision-cluster-links [data] (get-all-links data "files" "revision_clusters"))
+(defn file-revision-last-link [data] (get-last-link data "files" "revisions"))
+(defn revision-cluster-revision-last-link [data] (get-last-link data "files" "revisions"))
+
+
 ;; LayerVault API queries
 
 (def LAYERVAULT-API "http://api.layervault.com/api/v2/")
@@ -13,30 +34,54 @@
     (when id (str "/" id))
     "/?access_token=" access-token))
 
-(defn get-org-link-ids [data]
-  (get-in data ["users" 0 "links" "organizations"]))
+(defn query-exec-filter [filter callback data access-token]
+  (map
+    #(callback % access-token)
+    (filter data)))
 
-(defn get-project-link-ids [data]
-  (get-in data ["organizations" 0 "links" "projects"]))
+(defn query-exec [link-functions url id access-token]
+  (println (str (endpoint url id access-token)))
+  (let [data (-> (endpoint url id access-token)
+                 slurp
+                 json/read-str)]
+    (map
+      #(query-exec-filter % (get link-functions %) data access-token)
+      (keys link-functions))))
 
-(defn query-exec [callback url id filter access-token]
-  (map #(callback % access-token)
-    (-> (endpoint url id access-token)
-        slurp
-        json/read-str
-        filter)))
+(defn query-revision [id access-token]
+  (println (str "Querying revision..." id))
+  id)
+
+(defn query-revision-cluster [id access-token]
+  (println (str "Querying revision cluster..." id))
+  (query-exec {revision-cluster-revision-last-link query-revision}
+               "revision_clusters" id access-token))
+
+(defn query-file [id access-token]
+  (println (str "Querying file..." id))
+  (query-exec {file-revision-cluster-links query-revision-cluster
+               file-revision-last-link query-revision}
+               "files" id access-token))
+
+(defn query-folder [id access-token]
+  (println (str "Querying folder..." id))
+  (query-exec {folder-folder-links query-folder
+               folder-file-links query-file}
+               "folders" id access-token))
 
 (defn query-project [id access-token]
-  ;; print for now...
-  (println (str "Found project id: " id)))
+  (println (str "Querying project id... " id))
+  (query-exec {project-folder-links query-folder
+               project-file-links query-file}
+               "projects" id access-token))
 
 (defn query-org [id access-token]
   (println (str "Querying organization id..." id))
-  (query-exec query-project "/organizations" id get-project-link-ids access-token))
+  (query-exec {org-project-links query-project} "organizations" id access-token))
 
 (defn query-user [access-token]
   (println "Querying user...")
-  (query-exec query-org "/me" nil get-org-link-ids access-token))
+  (query-exec {user-org-links query-org} "me" nil access-token))
 
 
 ;; Entry point
