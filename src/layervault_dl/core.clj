481d6a9a5ab0b9f96be975670ec1efb2366ce391
Fravic <fravicf@gmail.com>
2015-03-22 01:53:20 -0700

    Get organization ids

--- src/layervault_dl/core.clj
+++ src/layervault_dl/core.clj
@@ -1,27 +1,33 @@
 (ns layervault-dl.core
   (:require [clojure.string :as string]
-            [org.httpkit.client :as http]
             [clojure.data.json :as json]
             [clojure.tools.cli :refer [parse-opts]]))
 
+;; LayerVault API queries
+
 (def LAYERVAULT-API "http://api.layervault.com/api/v2/")
 
-(def cli-options [])
+(defn endpoint
+  [endpoint id access-token]
+  (str LAYERVAULT-API endpoint
+    (when id (str "/" id))
+    "/?access_token=" access-token))
+
+(defn get-link-ids
+  [data]
+  (get-in data ["users" 0 "links" "organizations"]))
+
+(defn query-exec [callback url id access-token directory]
+  (map callback
+    (-> (endpoint url id access-token)
+        slurp
+        json/read-str
+        get-link-ids)))
+
+;; Entry point
+
 (def usage "layervault-dl ACCESS_TOKEN OUTPUT_DIR")
-
-(defn parse-user-data [data]
-  (pprint data))
-
-(defn query-user-data [access-token directory]
-  (let [options {:query-params {:access_token access-token}}]
-    (println options)
-    (http/get (str LAYERVAULT-API "me") options
-      (fn [{:keys [status headers body error]}]
-        (if error
-          (println "Failed! Error is" error)
-          (-> body
-            json/read-str
-            parse-user-data))))))
+(def cli-options [])
 
 (defn error-msg [errors]
   (str "The following errors occurred while parsing your command:\n\n"
@@ -37,4 +43,4 @@
     (cond
       (not= (count arguments) 2) (exit 1 usage)
       errors (exit 1 (error-msg errors)))
-    (query-user-data (first arguments) (second arguments))))
+    (query-exec pprint "/me" nil (first arguments) (second arguments))))
