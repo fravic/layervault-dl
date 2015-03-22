e27109027dbef11e00227e7bbbb2a1454ca31e7d
Fravic <fravicf@gmail.com>
2015-03-22 02:08:23 -0700

    Get project ids

--- src/layervault_dl/core.clj
+++ src/layervault_dl/core.clj
@@ -3,26 +3,41 @@
             [clojure.data.json :as json]
             [clojure.tools.cli :refer [parse-opts]]))
 
+
 ;; LayerVault API queries
 
 (def LAYERVAULT-API "http://api.layervault.com/api/v2/")
 
-(defn endpoint
-  [endpoint id access-token]
+(defn endpoint [endpoint id access-token]
   (str LAYERVAULT-API endpoint
     (when id (str "/" id))
     "/?access_token=" access-token))
 
-(defn get-link-ids
-  [data]
+(defn get-org-link-ids [data]
   (get-in data ["users" 0 "links" "organizations"]))
 
-(defn query-exec [callback url id access-token directory]
-  (map callback
+(defn get-project-link-ids [data]
+  (get-in data ["organizations" 0 "links" "projects"]))
+
+(defn query-exec [callback url id filter access-token]
+  (map #(callback % access-token)
     (-> (endpoint url id access-token)
         slurp
         json/read-str
-        get-link-ids)))
+        filter)))
+
+(defn query-project [id access-token]
+  ;; print for now...
+  (println (str "Found project id: " id)))
+
+(defn query-org [id access-token]
+  (println (str "Querying organization id..." id))
+  (query-exec query-project "/organizations" id get-project-link-ids access-token))
+
+(defn query-user [access-token]
+  (println "Querying user...")
+  (query-exec query-org "/me" nil get-org-link-ids access-token))
+
 
 ;; Entry point
 
@@ -43,4 +58,4 @@
     (cond
       (not= (count arguments) 2) (exit 1 usage)
       errors (exit 1 (error-msg errors)))
-    (query-exec pprint "/me" nil (first arguments) (second arguments))))
+    (query-user (first arguments))))
